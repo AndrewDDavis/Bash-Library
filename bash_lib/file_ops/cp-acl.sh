2e86f238c43f27f53604d860b2e5734e32860a57
Andrew Davis <addavis@gmail.com>
2025-07-28 11:59:06 -0400

    use ps_return

--- bash_lib/file_ops/cp-acl.sh
+++ bash_lib/file_ops/cp-acl.sh
@@ -1,3 +1,8 @@
+# dependencies
+import_func ps_return \
+    || return
+
+# docsh docs
 : """Copy file permissions and ACLs
 
     Usage: cp-acl <file 1> <file 2>
@@ -16,17 +21,9 @@ cp-acl() {
     sf_cmd=$( builtin type -P setfacl ) \
         || return 10
 
-    # use ps to capture return statuses adn return with max
-    local ps=()
+    # ps_return returns with max return status of the commands
+    "$gf_cmd" -p "$1" \
+        | "$sf_cmd" --set-file=- "$2"
 
-    "$gf_cmd" "$1" | "$sf_cmd" --set-file=- "$2"
-    ps=( "$PIPESTATUS[@]" )
-
-    local -i v rs=0
-    for v in "${ps[@]}"
-    do
-        (( v > rs )) && rs=$v
-    done
-
-    return $rs
+    ps_return
 }
