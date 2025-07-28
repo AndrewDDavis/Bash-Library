75b0b981e0698ddc11f2ed96f72059c3c44d95e0
Andrew Davis <addavis@gmail.com>
2025-07-28 11:55:35 -0400

    new docsh syntax

--- bash_lib/net_and_disks/ssh-rm_kh.sh
+++ bash_lib/net_and_disks/ssh-rm_kh.sh
@@ -1,15 +1,15 @@
+: """Remove entry from known_hosts.
+
+    Usage: ssh-rm_kh <name-or-IP>
+
+    - Useful e.g. when you know that the target host's IP has changed.
+    - Calls \`ssh-keygen -R ...\`.
+"""
+
 ssh-rm_kh() {
 
-    : """Remove entry from known_hosts.
-
-        Usage: ssh-rm_kh <name-or-IP>
-
-        - Useful e.g. when you know that the target host's IP has changed.
-        - Calls \`ssh-keygen -R ...\`.
-    """
-
-    [[ $# -eq 0 || $1 == @(-h|--help) ]] &&
-        { docsh -TD; return; }
+    [[ $# -eq 0 || $1 == @(-h|--help) ]] \
+        && { docsh -TD; return; }
 
     ssh-keygen -R "$1"
 }
--- bash_lib/net_and_disks/ssh-term.sh
+++ bash_lib/net_and_disks/ssh-term.sh
@@ -1,37 +1,37 @@
+: """Send TERM_PROGRAM, so tab titles can work properly on ChromeOS Terminal
+
+    Usage: ssh-term [options] user@host
+
+    Note on SSH command and shell logins:
+
+      - When running \`ssh host\`, sshd runs \`bash\` as a login shell.
+
+      - When running \`ssh host cmd\`, sshd runs \`bash -c cmd\`, not as a login shell
+        see [this QA for more detail](https://unix.stackexchange.com/q/744263/85414).
+
+      - Running \`exec -l ...\` replaces the non-login shell with a login shell, which
+        is preceeded by '-' in \$0, in the insane method that shells have for \"knowing\"
+        they're a login shell. Thus, running \`shopt -p | grep login\` will show that
+        login is set, and the shell sources /etc/profile and ~/.profile.
+
+      - OTOH, setting BASH_ARGV0 in the command call had the desired effect on \$0, but
+        the profile rc files were not read, and shopt shows it's not a login shell.
+
+      - In any case, the MOTD does not display in Ubuntu, but could be replicated by
+        adding \`run-parts /etc/update-motd.d/\` before the bash call. Or even just
+        replicating the header by sourcing /etc/lsb-release and then running:
+        printf \"Welcome to %s (%s %s %s)\\n\" \\
+            \"\$DISTRIB_DESCRIPTION\" \"\$( uname -o )\" \"\$( uname -r )\" \"\$( uname -m )\"
+
+      - This indicates that sshd, on user authentication, does more than just drop
+        the user into a shell. This may have to do with PAM authentication modules. See
+        the login process section of \`man sshd\` for details.
+"""
+
 ssh-term() {
 
-    : """Send TERM_PROGRAM, so tab titles can work properly on ChromeOS Terminal.
-
-        Usage: ssh-term [options] user@host
-
-        Note on SSH command and shell logins:
-
-          - When running \`ssh host\`, sshd runs \`bash\` as a login shell.
-
-          - When running \`ssh host cmd\`, sshd runs \`bash -c cmd\`, not as a login shell
-            see [this QA for more detail](https://unix.stackexchange.com/q/744263/85414).
-
-          - Running \`exec -l ...\` replaces the non-login shell with a login shell, which
-            is preceeded by '-' in \$0, in the insane method that shells have for \"knowing\"
-            they're a login shell. Thus, running \`shopt -p | grep login\` will show that
-            login is set, and the shell sources /etc/profile and ~/.profile.
-
-          - OTOH, setting BASH_ARGV0 in the command call had the desired effect on \$0, but
-            the profile rc files were not read, and shopt shows it's not a login shell.
-
-          - In any case, the MOTD does not display in Ubuntu, but could be replicated by
-            adding \`run-parts /etc/update-motd.d/\` before the bash call. Or even just
-            replicating the header by sourcing /etc/lsb-release and then running:
-            printf \"Welcome to %s (%s %s %s)\\n\" \\
-                \"\$DISTRIB_DESCRIPTION\" \"\$( uname -o )\" \"\$( uname -r )\" \"\$( uname -m )\"
-
-          - This indicates that sshd, on user authentication, does more than just drop
-            the user into a shell. This may have to do with PAM authentication modules. See
-            the login process section of \`man sshd\` for details.
-    """
-
-    [[ $# -eq 0  || $1 == @(-h|--help) ]] &&
-        { dosh -TD; return; }
+    [[ $# -eq 0  || $1 == @(-h|--help) ]] \
+        && { dosh -TD; return; }
 
     local ssh_cmd ssh_exec
 
