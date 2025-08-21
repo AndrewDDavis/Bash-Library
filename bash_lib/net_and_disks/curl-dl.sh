3c304ae63c77742d0ff2cb1ccb510138aefcd3c0
Andrew Davis <addavis@gmail.com>
2025-08-21 15:19:27 -0400

    make curl-dl a function with docs

--- bash_lib/net_and_disks/curl-dl.sh
+++ bash_lib/net_and_disks/curl-dl.sh
@@ -1,10 +1,19 @@
-# notable curl options
+# wcurl is an alternative that has more features and is distributed with curl
+alias curlw='wcurl'
+
+# docsh curl-dl
+# Download a file using 'curl -fLO'
+#
+# Usage: curl-dl <URL>
+#
+# Notable curl options:
+#
 #  -f : fail on HTTP error message > 400
 #  -L : follow server URL if it provides a moved location
 #  -O : output to a file, using the name from the URL
 #  -s : silent (no progress or errors)
 #  -S : print errors in silent mode
-alias curl-dl='curl -fLO'
+curl-dl() {
 
-# an alternative which has more features and is distributed with curl is wcurl
-alias curlw='wcurl'
+	curl -fLO "$@"
+}
