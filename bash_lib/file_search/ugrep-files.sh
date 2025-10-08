a48b2f3f3e08b4a349f0bcea50afa42f45ae0287
Andrew Davis <addavis@gmail.com>
2025-10-07 20:40:25 -0400

    - handle sigint - purge old code

--- bash_lib/file_search/ugrep-files.sh
+++ bash_lib/file_search/ugrep-files.sh
@@ -154,13 +154,17 @@ ugrep-files() {
         && { docsh -TD; return; }
 
     # err trap and clean up
+    trap 'return' ERR
+    trap 'return 130' SIGINT
     trap '
-        return
-    ' ERR
+        local c=$?
 
-    trap '
         unset -f _def_cmd _parse_opts _match_fns
-        trap - err return
+        trap - err int return
+
+        # reissue SIGINT if an interrupt triggered the return
+        (( c == 130 )) \
+            && kill -s SIGINT "$$"
     ' RETURN
 
     _def_cmd() {
@@ -241,12 +245,11 @@ ugrep-files() {
 
             # check ugrep return status from subprocess
             local grep_rs
-            wait $! \
-                || {
+            wait $!  || {
                 grep_rs=$?
-                [[ $grep_rs -eq 1 ]] \
-                    && return 1 \
-                    || { err_msg $grep_rs "grep command call error"; return; }
+                (( grep_rs == 1 )) && return 1
+                err_msg $grep_rs "grep command call error $grep_rs"
+                return
             }
         fi
 
@@ -272,215 +275,3 @@ ugrep-files() {
 
     _match_fns
 }
-
-
-### OLD CODE
-
-# the code below was developed before I started using ugrep, and is superceded
-# by ugrep-files. the discussion on ripgrep and git grep was moved to the 'File Search'
-# notes file.
-
-# grep-files() {
-
-#     [[ $# -eq 0 || $1 == @(-h|--help) ]] && {
-
-#         : """Search file contents, display matching files as list or tree
-
-#         Usage: grep-files [opts] <pattern> [path-root ...]
-
-#         The pattern is interpreted as extended regex (ERE). If no path-roots are given,
-#         the working directory is searched. Most other arguments are passed directly to
-#         \`grep\`, with the exception of the options listed below. To pass an option to
-#         \`grep\` that has the same name as one of \`grep-files\`'s options, include '--'
-#         as an argument before the option.
-
-#         By default, \`grep-files\` passes the '-l', '-R', and '-I' options to \`grep\`
-#         (described below). It calls the 'greps' shell function to achieve smart-case
-#         search functionality (i.e. case-insensitive search unless a non-escaped
-#         uppercase character appears in the pattern). This behaviour can be modified with
-#         '-i' or '-j' (refer to 'greps --help').
-
-#         Options interpreted by \`grep-files\`
-
-#           -a <regex>
-#           : add search for pattern using AND in file
-
-#           -J
-#           : treat binary files as grep does by default (see -I below)
-
-#           -t
-#           : search only text files (extensions .txt, .md, .adoc, .rtf, .tex, etc.)
-#             implies '-J'.
-
-#           -T
-#           : display file matches as tree
-
-
-#         Notable \`grep\` options
-
-#           -r, -R
-#           : search recursively within files and directories. '-R' will follow symlinks
-#             in the sub-paths.
-
-#           -l
-#           : print only the list of filenames, without matching lines
-
-#           -i
-#           : case insensitive search
-
-#           -w
-#           : match words only
-
-#           -x
-#           : match whole lines only
-
-#           -I
-#           : ignore binary files completely. Once grep determines that a file contains
-#             binary rather than text data (e.g. when a NUL byte is encountered), the rest
-#             of the file is skipped as if it contains no matches. By default, grep
-#             reports matches in binary files with a message to STDERR, but suppresses
-#             output. The default behaviour can be restored using '-J', which passes
-#             '--binary-files=binary' to grep.
-
-#           --exclude=GLOB, --include=GLOB
-#           : skip files whose base name matches GLOB, or search only those files
-
-
-#         Searching for multiple patterns:
-
-#         - To search for files that match any of several patterns (logical OR), it's
-#           trivial to use the pattern \`'A|B'\`, which is also the way grep interprets
-#           \`-e 'A' -e 'B'\`.
-
-#         - When searching for files that match multiple patterns on the same line, it's
-#           straightforward to use a pattern like 'A.*B|B.*A'. However, when the matches
-#           may be on different lines, multiple calls to grep must be chained together.
-#           This is the use case for the '-a' option to grep-files described above.
-#         "
-#         docsh -TD
-#         return
-#     }
-
-#     # clean up
-#     trap '
-#         unset -f _run_grep
-#         trap - return
-#     ' RETURN
-
-#     # defaults and args
-#     local and_pats=() grep_opts=( '-RlI' )
-#     local _t _verb=1 _tree=''
-#     local flag OPTARG OPTIND=1
-
-#     while getopts ':a:JtT' flag
-#     do
-#         case $flag in
-#             ( a )
-#                 and_pats+=( "$OPTARG" )
-#             ;;
-#             ( J )
-#                 grep_opts+=( "--binary-files=binary" )
-#             ;;
-#             ( t )
-#                 # text(-ish) files only
-#                 grep_opts+=( "--binary-files=binary" )
-#                 for _t in txt md text adoc ad asc asciidoc rtf tex
-#                 do
-#                     grep_opts+=( --include="*.$_t" )
-#                 done
-#             ;;
-#             ( T )
-#                 _tree=1
-#             ;;
-#             ( \? )
-#                 # other short option flags
-#                 grep_opts+=( -$OPTARG )
-#             ;;
-#             ( ??* )
-#                 # other long option flags
-#                 grep_opts+=( "--$flag" )
-#             ;;
-#             ( ':' )
-#                 err_msg 2 "-$OPTARG requires argument"
-#                 return
-#             ;;
-#         esac
-#     done
-#     shift $(( OPTIND - 1 ))
-
-
-#     _run_grep() (
-#         [[ $_verb -gt 0 ]] && set -x
-#         greps "${grep_opts[@]}" "$@"
-#     )
-
-#     # find matching files with grep
-#     # -t : remove trailing delim from each line
-#     # - nullify IFS to preserve leading and trailing whitespace in the fields
-#     # - not using process substitution '<(...)' to retain the return status
-#     local grep_out fns=()
-
-#     grep_out=$( _run_grep "$@" )
-#     readarray -t fns <<< "$grep_out"
-
-#     # Filter the results using AND patterns
-
-#     # TODO: the number of args before the final pattern can vary here!
-#     #       maybe need to process the args to find the pattern like egreps
-
-#     local _pat
-#     for _pat in "${and_pats[@]}"
-#     do
-#         readarray -t fns < <( _run_grep "${@:1:$#-2}" "$_pat" "${fns[@]}" )
-#     done
-
-
-#     # Print results, if any, as list or tree
-#     if [[ -z "${fns[@]}" ]]
-#     then
-#         #printf >&2 '%s\n' "No matches."
-#         return 1  # consistent with regular grep -q
-
-#     elif [[ -n ${_tree:-} ]]
-#     then
-#         local -a _tree_args=()
-
-#         # test for colour support
-#         if (( ${TERM_NCLRS:-2} >= 8 ))
-#         then
-#             _tree_args+=( "-C" )
-#         fi
-
-#         # sed script to trim the cruft of the --fromfile syntax
-#         local _sed_script
-#         _sed_script='# trim first and last lines
-#                      1 d; $ d
-
-#                      # trim the first four chars of most lines
-#                      # - root-dir lines get a newline prepended as well
-#                      # - brackets match space or no-break-sp (c2a0 in hex from hd -X)
-#                      /^.[^  ][^  ]./ { s/^..../\n/; b; }
-#                      /^.[  ][  ]./ { s/^....//; b; }
-#                     '
-
-#         tree -aFC --fromfile . <<< $( printf '%s\n' "${fns[@]}" ) |
-#             sed -E "$_sed_script"
-
-#     else
-#         # Ensure any alias for 'ls' gets used, e.g. for 'ls --color'
-#         # - this is the calling shell's alias
-#         local ls_cmd=( ls )
-
-#         if alias ls &>/dev/null
-#         then
-#             # word splitting that respects quotes
-#             str_to_words -q ls_cmd "${BASH_ALIASES[ls]}"
-
-#             # env is needed for aliases of the form 'KEY=val cmd'
-#             [[ ${ls_cmd[0]} == *=* ]] && ls_cmd=( env "${ls_cmd[@]}" )
-#         fi
-
-#         # NB -Q puts the file names in double-quotes, and escapes non-printables
-#         "${ls_cmd[@]}" -1dQ -- "${fns[@]}"
-#     fi
-# }
