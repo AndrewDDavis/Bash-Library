68bedc72ef32dfa52d033ffab6bb54031c4634a9
Andrew Davis <addavis@gmail.com>
2025-12-16 09:36:52 -0500

    avoid error message on non-container systems

--- bash_lib/sys_admin/lxcc.sh
+++ bash_lib/sys_admin/lxcc.sh
@@ -1,7 +1,17 @@
-# root priv required to test socket existence
-if sudo -n true
+# test whether we're inside a container:
+# - this works, but only root can read /proc/1/environ:
+#   sudo grep -a '^container=' /proc/1/environ
+#   # e.g.: container=lxc
+# - this works, but doesn't say what kind of container:
+#   grep kthreadd /proc/2/status 2>/dev/null
+#   # or
+#   ps -o comm= -p2 | grep kthreadd
+
+if ! grep -q kthreadd /proc/2/status 2>/dev/null
 then
-    if sudo /bin/test -e /root/lxd/termina_lxd.socket
+    # test socket existence (root required)
+    if sudo -n true 2>/dev/null \
+        && sudo /bin/test -e /root/lxd/termina_lxd.socket
     then
         lxcc() {
 
