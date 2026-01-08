a94d743aaafcd97a68dc2b036a8143578abd7638
Andrew Davis <addavis@gmail.com>
2026-01-08 14:43:08 -0500

    - allow script commands - refactor arg parsing - docs

--- bash_lib/docs/helpm.sh
+++ bash_lib/docs/helpm.sh
@@ -6,58 +6,78 @@
 # - completion
 # - scriptify?
 
+: """Display help pages for shell builtins, functions, or scripts
+
+    Usage
+
+        helpm <command>
+
+    If the command is a shell builtin or keyword, the help page is viewed using
+    the 'less' command or the value of \$PAGER. The 'help -m' command is used to
+    format the text similar to a man page, with some additional styling added by
+    helpm.
+
+    If the command is a function or script, 'docsh' is called to gather and view
+    the documentation.
+"""
+
 helpm() {
-
-    : """Display help pages for shell builtins or functions in a pager
-
-    Uses the value of \$PAGER or 'less', with formatting as if they're man pages. Uses
-    'docsh' to get the help text for functions.
-
-    Usage: helpm <command>
-    """
 
     [[ $# -eq 0 || $1 == @(-h|--help) ]] &&
         { docsh -TD; return; }
 
     [[ $# -gt 1 ]] &&
-        { err_msg 2 "too many args"; return; }
+        { err_msg 2 "too many args: '$*'"; return; }
 
-    local cmd types _b _f _k
-
+    local cmd
     cmd=$1
     shift
 
+
     # check command type
+    # `alias', `keyword', `function', `builtin', `file' or `'
+    local types _a _b _f _k _p
     types=$( builtin type -at "$cmd" )
 
-    command grep -q 'builtin' <<< "$types" && _b=1
-    command grep -q 'function' <<< "$types" && _f=1
-    command grep -q 'keyword' <<< "$types" && _k=1
+    [[ $types == *alias* ]] && _a=1
+    [[ $types == *builtin* ]] && _b=1
+    [[ $types == *function* ]] && _f=1
+    [[ $types == *keyword* ]] && _k=1
+    [[ $types == *file* ]] && _p=1
 
-    if [[ ( -n ${_b-} || -n ${_k-} )  &&  -n ${_f-} ]]
+    [[ -v _a ]] \
+        && err_msg w "'$cmd' refers to an alias"
+
+    if [[ ( -v _b || -v _k || -v _p )  &&  -v _f ]]
     then
-        err_msg w "'$cmd' is both a function and a shell builtin or keyword"
+        err_msg w "'$cmd' refers to both a function and a command"
 
-    elif [[ -z ${_b-}  && -z ${_f-}  && -z ${_k-} ]]
+    elif [[ ! ( -v _b || -v _f || -v _k || -v _p ) ]]
     then
         err_msg 2 "no matches for command: '$cmd'"
         return
     fi
 
-    if [[ -n ${_f-} ]]
+
+    # show help
+    if [[ -v _f ]]
     then
         # function
-        local docsh_txt
+        docsh -TDf "$cmd"
+    fi
 
-        if docsh_txt=$( docsh -TDf "$cmd" )
+    if [[ -v _p ]]
+    then
+        # command, possibly a script
+        if [[ $( command file -L "$( builtin type -P "$cmd" )" ) == *text* ]]
         then
-            command less -iR <<< "$docsh_txt"
+            docsh -TDp "$cmd"
         else
-            err_msg w "docsh error for function '$cmd'"
+            err_msg w "'$cmd' refers to a file, but it does not appear to be a script"
         fi
     fi
 
-    if [[ -n ${_b-}  || -n ${_k-} ]]
+    if [[ -v _b || -v _k ]]
     then
         # builtin or keyword
         local help_txt filt
@@ -74,7 +94,7 @@ helpm() {
 
         filt="s/^([[:upper:] ]+)\$/${_bld}\1${_rsb}/"
 
-        help_txt=$( sed -E "$filt" <<< "$help_txt" )
+        help_txt=$( command sed -E "$filt" <<< "$help_txt" )
 
         # NB, default user options for less will be respected, since they're in the
         # exported variable 'LESS', rather than an alias.
