ab6eb014c0f42182682963339d8f31c346bcc31d
Andrew Davis <addavis@gmail.com>
2026-02-02 16:14:58 -0500

    don't add -S if -s is used

--- bash_lib/net_and_disks/duh.sh
+++ bash_lib/net_and_disks/duh.sh
@@ -4,11 +4,17 @@ duh() {
 
     Usage: duh [du-options] [path-root]
 
-    This function prints the disk usage of directories under the specified tree, by
-    running the 'du -hcSD' command (option details below). If the path-root argument
-    is omitted, the current directory is used. The results are ordered using 'sort -h'.
+    This function prints the disk usage of directories under the specified
+    tree, by running the 'du -hSD' command (option details below). If the
+    path-root argument is omitted, the current directory is used. The results
+    are ordered using 'sort -h'.
 
-    To print file sizes as well as directory usage, use '-a' or a glob pattern.
+    - If the -s (--summarize) option is detected on the command line, the
+      -S option is omitted from the du command. This allows command-lines
+      like 'duh -s *' to act as intended.
+
+    - To print file sizes as well as directory usage, use '-a' or a glob
+      pattern.
 
     Notable du options:
 
@@ -43,6 +49,22 @@ duh() {
 	[[ $# -eq 1  && $1 == @(-h|--help) ]] &&
     	{ docsh -TD; return; }
 
-    command du -hcSD "$@" \
+    # rudimentary check for -s
+    local arg _s
+    for arg in "$@"
+    do
+        [[ $arg == -*s* ]] \
+            && _s=1
+    done
+
+    local du_args=()
+    if [[ -v _s ]]
+    then
+        du_args+=( -hD )
+    else
+        du_args+=( -hSD )
+    fi
+
+    command du "${du_args[@]}" "$@" \
         | command sort -h
 }
