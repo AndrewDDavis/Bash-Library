fde3732990c98656b88f839dff0df54e4e578754
Andrew Davis <addavis@gmail.com>
2026-07-11 20:19:01 -0400

    move docs out of func defn

--- lib/file_search/fd-wrapper.sh
+++ lib/file_search/fd-wrapper.sh
@@ -1,117 +1,139 @@
-# aliases are also defined in the env/ dir
+# dependencies
 import_func array_strrepl tree-fromfiles \
     || return
 
-fd-wrapper() {
+# docsh fd-wrapper
+# Find files using user-friendly syntax and defaults
+#
+# Usage: fd-wrapper [options] [pattern] [path ...]
+#
+# This function acts as a wrapper to the fd command, which may be called fdfind on
+# some systems. The --no-ignore-vcs option is added to the fd command by default,
+# and the following additional option is interpreted:
+#
+# --tree
+# : This option shows the matched files in a tree view. This is done by passing
+#   the fd output as a null-delimited list to the tree-fromfiles function, which
+#   relies on the tree command. The --noreport tree option is also recognized.
+#
+# The partner file fd-wrapper_env.sh defines an alias of fd to fd-wrapper, as well
+# as convenience aliases such as fdtree, fda, fdl, and fdu.
+#
+# If the path is omitted, the tree under the current directory is searched.
+#
+# Symlinks are not followed, unless -L (--follow) is used. Mount points in the
+# tree are searched, unless --xdev (--one-file-system) is used.
+#
+# The pattern is interpreted as a regular expression that may match a substring of
+# file basenames. The Rust regex engine is used, with syntax similar to 'grep -E'
+# or POSIX ERE (refer to <https://docs.rs/regex/1.0.0/regex/#syntax>). The
+# following fd options modify the pattern matching behaviour:
+#
+# -p (--full-path)
+# : Match full paths.
+#
+# -F (--fixed-strings)
+# : Treat the pattern as a literal string, but still match as a substring.
+#
+# -g (--glob)
+# : Treat the pattern as a glob. This option also disables substring matching
+#   (only full filenames are matched). If combined with --full-path, '**' matches
+#   multiple path components.
+#
+# --and
+# : Add additional patterns which must both match a filename. To add alternative
+#   (OR) patterns, use syntax like 'abc|def'.
+#
+# By default, fd uses smart-case matching (i.e. cases-insensitive for lowercase
+# patterns). Modify this with -s (--case-sensitive) or -i (--ignore-case).
+#
+# By default, fd excludes hidden files and directories from the search results
+# (i.e. filenames that start with '.'). It also excludes files designated in
+# ignore files noted below. To show all possible results, use option -u
+# (--unrestricted), which is an alias for -HI. The following options may be used
+# to include specific file types:
+#
+# -H (--hidden)
+# : Include hidden files in the search results (i.e. filenames beginning with
+#   '.').
+#
+# -I (--no-ignore)
+# : Include files that would be excluded by ignore files. fd recognizes git ignore
+#   files at the directory, repository, and global levels: '.gitignore',
+#   '.git/info/exclude', and '~/.config/git/ignore'. It also respects directory-
+#   level ignore files named '.ignore' and '.fdignore', and a global ignore file
+#   at '~/.config/fd/ignore'.
+#
+#   Refer to the gitignore manpage for the syntax of those files, which is similar
+#   to shell globbing that includes the '**' pattern.
+#
+#   It's generally useful to create a '~/.config/fd/ignore' file that excludes the
+#   ignore files themselves, the contents of .git dirs, and other hidden files of
+#   little interest.
+#
+# --no-ignore-vcs
+# : Show results that would be excluded by the git ignore files, but respect the
+#   fd-specific ones. This option is added automatically by fd-wrapper. Use
+#   --ignore-vcs to override the function default.
+#
+# The search results may be filtered using these options:
+#
+# -E (--exclude)
+# : Excluded files using a glob pattern. E.g. '--exclude \*.pyc'.
+#
+# -t (--type)
+# : Include only specific file type(s), which accepts d for dirs, f for files, l
+#   for symlinks, x for executable files, or e for empty files. E.g. '-te -td' for
+#   empty dirs.
+#
+# -e (--extension)
+# : Include only specific file extension(s). Repeated use of this option matches
+#   multiple possible extensions, e.g. '-e .sh -e .bash'. The dot may be omitted
+#   from the pattern, but will be added internally. To match a filename suffix
+#   that is not preceded by a dot, use a regular pattern such as '_env.sh$'. To
+#   match files with no extension, use the regex pattern '^[^.]+$'.
+#
+# --newer (--changed-after, --change-newer-than, --changed-within)
+# --older (--changed-before, --change-older-than)
+# : These filters accept a duration (e.g. 10h, 1d, 35min, 2weeks) or a specific
+#   date or time (e.g. YYYY-MM-DD).
+#
+# -S (--size)
+# : Filter based on file size, as <+-><NUM><UNIT>, e.g. '-5k'.
+#
+# -o (--owner)
+# : Filter base on owner, as [user][:group].
+#
+# The output from fd and tree is colorized using LS_COLORS, and may be modified by
+# these options:
+#
+#   - Use -l (--list-details) to print a detailed listing, like 'ls -l'.
+#
+#   - Use -0, (--print0) to print a null character between search results, rather
+#     than a newline.
+#
+#   - Use --format for custom output.
+#
+# Use -x (--exec) command [args...] to execute a command for each result, using
+# parallelized search and execution. To execute the command once, with all results
+# as arguments, use -X (--exec-batch).
+#
+# Examples
+#
+#   - Match all files in a directory:
+#
+#     fd -HI . dir/
+#
+#   - Equivalent to \"find * -name '*.txt'\":
+#
+#     fd -e .txt
+#
+#   - Show the disk usage of all Trash, .Trash, and .Trash-1000 folders:
+#
+#     fd -H '^(\\.)?Trash(-[0-9]+)?' ~/ \\
+#         -X du -hsc | sort -h
 
-    # Find variant with user-friendly syntax and defaults
-    #
-    # Usage: fd-wrapper [options] [pattern] [path ...]
-    #
-    # This function matches filenames using the fd command, which may actually be called
-    # fdfind on some systems. The --no-ignore-vcs option is added by default. This
-    # function recognizes the --tree option (details below), but otherwise operates just
-    # like calling the fd command directly.
-    #
-    #   --tree
-    #   : This option shows the matched files in a tree view. This is done by passing the
-    #     fd output as a null-delimited list to the tree-fromfiles function, which relies
-    #     on the tree command. The --noreport tree option is also recognized.
-    #
-    # Pattern matching occurs according to the following behaviour:
-    #
-    #   - If the path is omitted, the tree under the current directory is searched.
-    #     Symlinks are not followed, unless -L (--follow) is used. Mount points in the
-    #     tree are searched, unless --xdev (--one-file-system) is used.
-    #
-    #   - The pattern is interpreted as a regular expression that may match a substring
-    #     of file basenames. The Rust regex engine is used, with syntax similar to
-    #     'grep -E' or POSIX ERE (<https://docs.rs/regex/1.0.0/regex/#syntax>).
-    #
-    #       + Use -p (--full-path) to match full paths.
-    #
-    #       + Use -F (--fixed-strings) to treat the pattern as a literal string, but
-    #         still match as a substring.
-    #
-    #       + Use -g (--glob) for glob pattern matching. This option also disables
-    #         substring matching (exact filenames are matched). If combined with
-    #         --full-path, '**' matches multiple path components.
-    #
-    #       + Use --and to add additional patterns which must both match a filename. To
-    #         add alternative (OR) patterns, use syntax like 'abc|def'.
-    #
-    #   - Smart-case matching is employed (cases-insensitive for lowercase patterns).
-    #
-    #       + Modify this with -s (--case-sensitive) or -i (--ignore-case).
-    #
-    #   - By default, fd excludes hidden files (filenames that start with '.'), as well
-    #     as other ignored files noted below.
-    #
-    #       + Use -H (--hidden) to show hidden files. Use -I (--no-ignore) to disable all
-    #         ignore files (i.e. show more results). Option -u (--unrestricted) is an
-    #         alias for -HI.
-    #
-    #       + By default, fd respects git-ignore files at the directory, repository, and
-    #         global levels: '.gitignore', '.git/info/exclude', and '~/.config/git/ignore'.
-    #         It also respects directory-level files named '.ignore' and '.fdignore', and
-    #         a global ignore file at '~/.config/fd/ignore'.
-    #
-    #         Refer to the gitignore manpage for the syntax of those files, which is
-    #         roughly shell globbing that includes the '**' pattern.
-    #
-    #       + This function adds the --no-ignore-vcs option to the command line. This
-    #         shows results that would be excluded by the git-ignore files, but still
-    #         respects the fd-specific ones. Use --ignore-vcs to override the function
-    #         default.
-    #
-    #       + It's generally useful to create a '~/.config/fd/ignore' file that excludes
-    #         the ignore files themselves, the contents of .git dirs, and other hidden
-    #         files of little interest.
-    #
-    #   - The search results may be filtered using these options:
-    #
-    #       + -E (--exclude), which takes a glob pattern.
-    #
-    #       + -t (--type), which accepts d for dirs, f for files, l for symlinks, x for
-    #         executable files, or e for empty files. E.g. '-te -td' for empty dirs.
-    #
-    #       + -e (--extension), to filter by file extension. To match files with no
-    #         extension, use the regex pattern '^[^.]+$'.
-    #
-    #       + Time-based options, such as --newer, --older, --changed-within,
-    #         --changed-after, --changed-before, which take a duration (e.g. 10h, 1d,
-    #         35min) or a specific date or time (e.g. YYYY-MM-DD).
-    #
-    #       + Other properties such as -S (--size) and -o (--owner) [user][:group].
-    #
-    # The output from fd and tree is colorized using LS_COLORS, and may be modified by
-    # these options:
-    #
-    #   - Use -l (--list-details) to print a detailed listing, like 'ls -l'.
-    #
-    #   - Use -0, (--print0) to print a null character between search results, rather
-    #     than a newline.
-    #
-    #   - Use --format for custom output.
-    #
-    # Use -x (--exec) command [args...] to execute a command for each result, using
-    # parallelized search and execution. To execute the command once, with all results
-    # as arguments, use -X (--exec-batch).
-    #
-    # Examples
-    #
-    #   - Match all files in a directory:
-    #
-    #     fd -HI . dir/
-    #
-    #   - Equivalent to \"find * -name '*.txt'\":
-    #
-    #     fd -e .txt
-    #
-    #   - Show the disk usage of all Trash, .Trash, and .Trash-1000 folders:
-    #
-    #     fd -H '^(\\.)?Trash(-[0-9]+)?' ~/ \\
-    #         -X du -hsc | sort -h
+fd-wrapper() {
 
     # fd or fdfind command path and default args
     local fd_cmd
