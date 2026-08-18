f7cce9ccecd68dab73e9492157b7b130713dcc01
Andrew Davis <addavis@gmail.com>
2026-08-18 14:54:50 -0400

    refactor: split into funcs

--- lib/shell_interactive/func-where.sh
+++ lib/shell_interactive/func-where.sh
@@ -37,124 +37,144 @@ func-where() {
 
     # clean-up trap
     trap '
-        unset -f _func_not_found
+        unset -f _get_func_info _func_not_found _check_skel _vrb_run _check_srcln
         trap - return
     ' RETURN
+
+    _get_func_info() {
+
+        # gather func source fn and line
+
+        # define regex separately to avoid shell quoting issues
+        # - pattern matches function name, line number, and source file path
+        # - e.g. for the_func() defined in 'a func.sh', declare -F would output:
+        #   'the_func 1 a func.sh'
+        # - then, [[ $sss =~ ^([^ ]+)\ ([0-9]+)\ (.+)$ ]] would produce:
+        #   BASH_REMATCH=([0]="the_func 1 a func.sh" [1]="the_func" [2]="1" [3]="a func.sh")
+        local regex_ptn='^([^ ]+) ([0-9]+) (.+)$'
+
+        local dec_out
+        dec_out=$( shopt -s extdebug; declare -F "$func_nm" ) \
+            || { _func_not_found; continue; }
+
+        [[ $dec_out =~ $regex_ptn ]] \
+            || { err_msg 6 "dec_out did not match regex"; return; }
+
+        src_fn=${BASH_REMATCH[3]}
+        src_ln=${BASH_REMATCH[2]}
+    }
 
     _func_not_found() {
 
         # not a function; check for alias
-        if dec_out=$( alias "$func_nm" 2>/dev/null )
+        local al_out
+        if al_out=$( alias "$func_nm" 2>/dev/null )
         then
-            err_msg 5 "not a function, but found alias: ${dec_out#*=}"
+            err_msg 5 "not a function, but found alias: ${al_out#*=}"
         else
             err_msg 6 "function not found: '$func_nm'"
         fi
     }
 
-    # # record state of extdebug, and ensure it's enabled
-    # - now setting in the subshell with declare
-    # local _ed_keepon
-    # if shopt extdebug >/dev/null
-    # then
-    #     _ed_keepon=1
-    # else
-    #     shopt -s extdebug
-    # fi
+    _check_skel() {
 
+        if [[ $src_fn == *import_func.sh  && $func_nm != import_func ]]
+        then
+            # func defn is a skel_func (placeholder function definition)
+            local skel_defn regex_ptn libdir
+            skel_defn=$( builtin declare -pf "$func_nm" )
 
-    # Gather func defn info
+            regex_ptn='import_func -d "(.*)" -f'
 
-    # define regex separately to avoid shell quoting issues
-    # - pattern matches function name, line number, and source file path
-    # - e.g. for the_func() defined in 'a func.sh', declare -F would output:
-    #   'the_func 1 a func.sh'
-    # - then, [[ $sss =~ ^([^ ]+)\ ([0-9]+)\ (.+)$ ]] would produce:
-    #   BASH_REMATCH=([0]="the_func 1 a func.sh" [1]="the_func" [2]="1" [3]="a func.sh")
-    local regex_ptn='^([^ ]+) ([0-9]+) (.+)$'
+            [[ $skel_defn =~ $regex_ptn ]] \
+                || { err_msg 7 "skel_defn did not match regex"; return; }
 
-    local func_nm dec_out src_fn src_ln
+            libdir=${BASH_REMATCH[1]}
+
+            # evaluate import_func with the lib_dir from the skel func
+            import_func -d "$libdir" -f "$func_nm"
+
+            _get_func_info
+        fi
+    }
+
+    _vrb_run() {
+        local cmd=( "$@" )
+        printf >&2 '%s\n' "${PS4}${cmd[*]}"
+        "${cmd[@]}"
+    }
+
+    _check_srcln() {
+
+        # check source line
+        # - NB, if there a sub-functions defined within the function, declare -F may
+        #   return the line-no for one of those! Yikes, better check.
+        # - per bash manpage:
+        #   fname () compound-command [redirection]
+        #   function fname [()] compound-command [redirection]
+        # - a compound command is surrounded by {...}, (...), [[...]], or ((...))
+        # - bash-completion package uses:
+        #   _comp_abspath()
+        #   {
+        #   ...
+        local funcdef_ptn="^[[:blank:]]*("
+        funcdef_ptn+="${func_nm}[[:blank:]]*\\(\\)"
+        funcdef_ptn+="|function[[:blank:]]+${func_nm}([[:blank:]]*\\(\\))?"
+        funcdef_ptn+=")([[:blank:]]*[{([]|\$)"
+
+        # - read source file lines into array
+        local -a src_lines
+        mapfile -t -O1 src_lines < "$src_fn"
+
+        if [[ -z ${src_lines[src_ln]-} ]] \
+            || [[ ! ${src_lines[src_ln]} =~ $funcdef_ptn ]]
+        then
+            # search for the correct line
+            local i m
+            for (( i=1; i<=${#src_lines[*]}; i++ ))
+            do
+                [[ ${src_lines[i]} =~ $funcdef_ptn ]] && {
+                    src_ln=$i
+                    m=1
+                    break
+                }
+            done
+
+            [[ -v m ]] \
+                || { err_msg 9 "func defn for '$func_nm' not found at line $src_ln of '$src_fn': ${src_lines[src_ln]}"; return; }
+        fi
+    }
+
+    local func_nm src_fn src_ln
     for func_nm in "$@"
     do
-        dec_out=$( shopt -s extdebug; declare -F "$func_nm" ) \
-            || { _func_not_found; continue; }
-
-        [[ $dec_out =~ $regex_ptn ]]
-        src_fn=${BASH_REMATCH[3]}
-        src_ln=${BASH_REMATCH[2]}
+        # gather func defn info, and make sure it's not a placeholder
+        _get_func_info \
+            || continue
+        _check_skel \
+            || continue
 
         if [[ -v _s ]]
         then
-            # source
-            local src_cmd=( builtin source "$src_fn" )
-            printf >&2 '%s\n' "${PS4}${src_cmd[*]}"
-
-            # vvv disabled, since extdebug is only in the subshell
-            # # NB, if import_func runs in the sourced file, it will complain about
-            # # functrace (from extdebug)
-            # shopt -u extdebug
-
-            "${src_cmd[@]}"
-
-            # shopt -s extdebug
+            # source the function definition file
+            _vrb_run builtin source "$src_fn"
 
         elif [[ -v _e ]]
         then
-            # edit
-            local edt_cmd=( "$EDITOR" "$src_fn" )
-            printf >&2 '%s\n' "${PS4}${edt_cmd[*]}"
-            "${edt_cmd[@]}"
+            # open source file in editor
+            _vrb_run "$EDITOR" "$src_fn"
 
         else
-            # print
+            # print source filename and line
 
-            # check source line
-            # - NB, if there a sub-functions defined within the function, declare -F may
-            #   return the line-no for one of those! Yikes, better check.
-            # - per bash manpage:
-            #   fname () compound-command [redirection]
-            #   function fname [()] compound-command [redirection]
-            # - a compound command is surrounded by {...}, (...), [[...]], or ((...))
-            # - bash-completion package uses:
-            #   _comp_abspath()
-            #   {
-            #   ...
-            local funcdef_ptn="^[[:blank:]]*("
-            funcdef_ptn+="${func_nm}[[:blank:]]*\\(\\)"
-            funcdef_ptn+="|function[[:blank:]]+${func_nm}([[:blank:]]*\\(\\))?"
-            funcdef_ptn+=")([[:blank:]]*[{([]|\$)"
+            _check_srcln \
+                || continue
 
-            # - read source lines into array
-            local -a src_lines
-            mapfile -t -O1 src_lines < "$src_fn"
-
-            if [[ -z ${src_lines[src_ln]-} ]] \
-                || [[ ! ${src_lines[src_ln]} =~ $funcdef_ptn ]]
-            then
-                # search for the correct line
-                local i m
-                for (( i=1; i<=${#src_lines[*]}; i++ ))
-                do
-                    [[ ${src_lines[i]} =~ $funcdef_ptn ]] && {
-                        src_ln=$i
-                        m=1
-                        break
-                    }
-                done
-
-                [[ -v m ]] \
-                    || { err_msg 9 "func defn for '$func_nm' not found at line $src_ln of '$src_fn': ${src_lines[src_ln]}"; return; }
-            fi
-
-            # - grep-style context
+            # grep-style context
             [[ $# -gt 1 ]] &&
                 printf '%s' "${func_nm}: "
 
             printf '%s\n' "ln. $src_ln in '$src_fn'"
         fi
     done
-
-    # # reset extdebug
-    # [[ -v _ed_keepon ]] \
-    #     || shopt -u extdebug
 }
